59bc5ce5bb04482366a349321975dc4272bceacc
pressfforyasuo <72881304+pressfforyasuo@users.noreply.github.com>
2023-05-20 18:28:22 +0300

    Initial commit

---
+++ WebApp/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace WebApp
+{
+    [Route("api")]
+    public class LoginController : Controller
+    {
+        private readonly IAccountDatabase _db;
+
+        public LoginController(IAccountDatabase db)
+        {
+            _db = db;
+        }
+
+        [HttpPost("sign-in")]
+        public async Task<IActionResult> Login(string userName)
+        {
+            //TODO 1: Generate auth cookie for user 'userName' with external id ?
+            var account = await _db.FindByUserNameAsync(userName);
+            if (account != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, account.ExternalId),
+                    new Claim(ClaimTypes.Role, account.Role)
+                };
+                var userId = new ClaimsIdentity(claims, "Cookie");
+                await HttpContext.SignInAsync("Cookie", new ClaimsPrincipal(userId));
+                return Ok();
+            }
+            // TODO 2: Return 404 if user not found ?
+            return NotFound();
+        }
+    }
+}
---
+++ WebApp/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using System;
+using System.Security.Claims;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        // TODO 0: Something broken in ConfigureServices.
+        // Don't focus attention on it right now, you will faced with problem in process of meeting the challenges TODOs
+        // Pay attention to different possible solutions of the problem
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddSingleton<IAccountDatabase, AccountDatabaseStub>();
+            services.AddSingleton<IAccountCache, AccountCache>();
+            services.AddSingleton<IAccountService, AccountService>();
+
+            services.AddAuthentication("Cookie").AddCookie("Cookie", options => {
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = 401;
+                    return Task.CompletedTask;
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Admin", builder =>
+                {
+                    builder.RequireClaim(ClaimTypes.Role, "Admin");
+                });
+                options.AddPolicy("user", builder =>
+                {
+                    builder.RequireClaim(ClaimTypes.Role, "user");
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseMvc();
+
+            app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+        }
+    }
+}
